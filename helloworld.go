3e91eb0cf04577e3e43b8165766d58927914b846
guigmarconi <30665498+guigmarconi@users.noreply.github.com>
2025-05-07 00:57:27 -0300

    final changes to HelloWorld

--- helloworld.go
+++ helloworld.go
@@ -1,18 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-const helloPrefix = "Hello, "
+const (
+	portuguese = "Portuguese"
+	french     = "French"
+	spanish    = "Spanish"
 
-func Hello(name string) string {
+	englishHelloPrefix = "Hello, "
+	portuguesePrefix   = "Ola, "
+	frenchPrefix       = "Bonjour, "
+	spaninshPrefix     = "Hola, "
+)
+
+func Hello(name, language string) string {
 
 	if name == "" {
 		name = "World"
 	}
 
-	return helloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+
+	switch language {
+	case portuguese:
+		prefix = portuguesePrefix
+	case french:
+		prefix = frenchPrefix
+	case spanish:
+		prefix = spaninshPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Guigo"))
+	fmt.Println(Hello("Gustave", portuguese))
 }
